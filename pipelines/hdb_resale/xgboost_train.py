87f4833ea20af25bc41b3eed20d2d6ee40ae8ec9
teyang-lau <ty_lau@outlook.com>
2023-06-04 19:56:36 +0800

    updated parser arguments

--- pipelines/hdb_resale/xgboost_train.py
+++ pipelines/hdb_resale/xgboost_train.py
@@ -22,13 +22,15 @@ parser.add_argument("--min_child_weight", type=int, default=6)
 parser.add_argument("--subsample", type=float, default=0.9)
 parser.add_argument("--objective", type=str, default="reg:squarederror")
 parser.add_argument("--eval_metric", type=str, default="mae")
-parser.add_argument("--nfold", type=int, default=3)
+parser.add_argument("--nfold", type=int, default=2)  # 3
 parser.add_argument("--early_stopping_rounds", type=int, default=3)
 
 # Set location of input training data
-parser.add_argument("--train", type=str)
+parser.add_argument("--train", type=str, default=os.environ.get("SM_CHANNEL_TRAIN"))
 # Set location of input validation data
-parser.add_argument("--validation", type=str)
+parser.add_argument(
+    "--validation", type=str, default=os.environ.get("SM_CHANNEL_VALIDATION")
+)
 # Set location where trained model will be stored. Default set by SageMaker, /opt/ml/model
 parser.add_argument("--model_dir", type=str, default=os.environ.get("SM_MODEL_DIR"))
 # Set location where model artifacts will be stored. Default set by SageMaker, /opt/ml/output/data
@@ -40,13 +42,13 @@ args = parser.parse_args()
 print(args)
 
 logger.debug("Reading train data.")
-data_train = pd.read_csv(f"{args.train_data_dir}/train.csv")
+data_train = pd.read_csv(f"{args.train}/train.csv")
 train = data_train.drop("resale_price", axis=1)
 label_train = pd.DataFrame(data_train["resale_price"])
 dtrain = xgb.DMatrix(train, label=label_train)
 
 logger.debug("Reading validation data.")
-data_val = pd.read_csv(f"{args.val_data_dir}/validation.csv")
+data_val = pd.read_csv(f"{args.validation}/validation.csv")
 val = data_val.drop("resale_price", axis=1)
 label_val = pd.DataFrame(data_val["resale_price"])
 dval = xgb.DMatrix(val, label=label_val)
